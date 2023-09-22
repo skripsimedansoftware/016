790fa7e2dc5d1be7ab9985f94c581645bc58a310
Agung Dirgantara <agungmasda29@gmail.com>
2023-09-22 20:05:37 +0700

    array of items

--- validators/pendaftaran/step-4.js
+++ validators/pendaftaran/step-4.js
@@ -45,16 +45,18 @@ module.exports = [
     .custom((_value, { req }) => {
       const imageExtensions = ['jpg', 'jpeg', 'png'];
 
-      if (req.files?.fotocopy_izin_usaha) {
-        const fileExt = mime.getExtension(req.files.fotocopy_izin_usaha[0].mimetype);
-        if (imageExtensions.indexOf(fileExt) !== -1) {
-          fs.renameSync(req.files.fotocopy_izin_usaha[0].path, `${req.files.fotocopy_izin_usaha[0].path}.${fileExt}`);
-          req.files.fotocopy_izin_usaha[0].filename = `${req.files.fotocopy_izin_usaha[0].filename}.${fileExt}`;
-          return true;
-        }
+      if (req.files?.foto_produksi) {
+        req.files.foto_produksi.map((item) => {
+          const fileExt = mime.getExtension(item.mimetype);
+          if (imageExtensions.indexOf(fileExt) !== -1) {
+            fs.renameSync(item.path, `${item.path}.${fileExt}`);
+            item.filename = `${item.filename}.${fileExt}`;
+            return item;
+          }
 
-        fs.rmSync(req.files.fotocopy_izin_usaha[0].path);
-        throw new Error('File harus berupa gambar');
+          fs.rmSync(item.path);
+          throw new Error('File harus berupa gambar');
+        });
       }
 
       throw new Error('Pilih berkas foto produksi usaha Anda');
